87cbb83fc692f69d9331cb82181521cda7f32105
webkey <webkey_i@ukr.net>
2016-08-18 19:57:06 +0300

    of_20160818_1957

--- js/functions.js
+++ js/functions.js
@@ -36,6 +36,234 @@ function printShow() {
 }
 /*print end*/
 
+/*main navigation*/
+(function ($) {
+	// external js:
+	// 1) TweetMax VERSION: 1.19.0 (widgets.js);
+	// 2) device.js 0.2.7 (widgets.js);
+	// 3) resizeByWidth (resize only width);
+	var MainNavigation = function (settings) {
+		var options = $.extend({
+			navContainer: null,
+			navMenu: '.nav-list',
+			btnMenu: '.btn-menu',
+			btnClose: '.btn-nav-close',
+			navMenuItem: '.nav-list > li',
+			navMenuAnchor: 'a',
+			navDropMenu: '.js-nav-drop',
+			overlayClass: '.nav-overlay',
+			overlayAppend: 'body',
+			overlayAlpha: 0.8,
+			classNoClickDrop: '.no-click', // Класс, при наличии которого дроп не буте открываться по клику
+			classReturn: null,
+			overlayBoolean: false,
+			animationSpeed: 300,
+			animationSpeedOverlay: null,
+			minWidthItem: 100
+		},settings || {});
+
+		var self = this,
+			container = $(options.navContainer),
+			_animateSpeed = options.animationSpeed;
+
+		self.options = options;
+		self.$navContainer = container;
+		self.$navMenu = $(options.navMenu);
+		self.$btnMenu = $(options.btnMenu);                        // Кнопка открытия/закрытия меню для моб. верси;
+		self.$btnClose = $(options.btnClose);                      // Кнопка закрытия меню для моб. верси;
+		self.$navMenuItem = $(options.navMenuItem, container);     // Пункты навигации;
+		self.$navMenuAnchor = $(options.navMenuAnchor, container); // Элемент, по которому производится событие (клик);
+		self.$navDropMenu = $(options.navDropMenu, container);     // Дроп-меню всех уровней;
+		self._animateSpeed = _animateSpeed;
+		self._classNoClick = options.classNoClickDrop;
+
+		// overlay
+		self._overlayBoolean = options.overlayBoolean;            // Добавить оверлей (по-умолчанию == false). Если не true, то не будет работать по клику вне навигации;
+		self._overlayClass = options.overlayClass;                // Класс оверлея;
+		self.overlayAppend = options.overlayAppend;                // Элемент ДОМ, вконец которого добавится оверлей, по умолчанию <body></body>;
+		self.$overlay = $('<div class="' + self._overlayClass.substring(1) + '"></div>'); // Темплейт оверлея;
+		self._overlayAlpha = options.overlayAlpha;
+		self._animateSpeedOverlay = options.animationSpeedOverlay || _animateSpeed;
+
+		self._minWidthItem = options.minWidthItem;
+
+		self.desktop = device.desktop();
+
+		self.modifiers = {
+			active: 'active',
+			hover: 'hover',
+			opened: 'nav-opened',
+			position: 'position',
+			current: 'current',
+			alignRight: 'align-right'
+		};
+
+		self.createOverlay();
+		self.initNavTween();
+		self.switchNav();
+		self.clearStyles();
+	};
+
+	// init tween animation
+	MainNavigation.prototype.overlayTween = new TimelineMax({paused: true});
+	MainNavigation.prototype.navTween = new TimelineMax({paused: true});
+
+	// overlay append to "overlayAppend"
+	MainNavigation.prototype.createOverlay = function () {
+		var self = this;
+		if (!self._overlayBoolean) return false;
+
+		var $overlay = self.$overlay;
+		$overlay.appendTo(self.overlayAppend);
+
+		TweenMax.set($overlay, {autoAlpha: 0});
+
+		self.overlayTween.to($overlay, self._animateSpeedOverlay / 1000, {autoAlpha: self._overlayAlpha});
+	};
+
+	// show/hide overlay
+	MainNavigation.prototype.showOverlay = function (close) {
+		var self = this;
+		if (!self._overlayBoolean) return false;
+
+		var overlayTween = self.overlayTween;
+
+		if (close === false) {
+			overlayTween.reverse();
+			return false;
+		}
+
+		if (overlayTween.progress() != 0 && !overlayTween.reversed()) {
+			overlayTween.reverse();
+			return false;
+		}
+
+		overlayTween.play();
+	};
+
+	// init nav tween
+	MainNavigation.prototype.initNavTween = function () {
+		var self = this,
+			_animationSpeed = self._animateSpeedOverlay,
+			$navContainer = self.$navContainer,
+			_translateValue = $(window).outerHeight();
+
+		self.navTween
+			.to($navContainer, _animationSpeed/1000, {y:0, ease:Cubic.easeInOut});
+	};
+
+	// switch nav
+	MainNavigation.prototype.switchNav = function () {
+		var self = this,
+			$html = $('html'),
+			$buttonMenu = self.$btnMenu,
+			modifiers = self.modifiers,
+			_activeClass = modifiers.active;
+
+		$buttonMenu.on('click', function (e) {
+			var thisBtnMenu = $(this);
+
+			if (thisBtnMenu.hasClass(_activeClass)) {
+				self.closeNav($html,$buttonMenu);
+			} else {
+				self.openNav($html,$buttonMenu);
+			}
+
+			e.preventDefault();
+		});
+
+		$(document).on('click', self._overlayClass, function () {
+			self.closeNav($html,$buttonMenu);
+		});
+
+		$(window).on('resizeByWidth', function () {
+			self.closeNav($html,$buttonMenu);
+		});
+	};
+
+	// open nav
+	MainNavigation.prototype.openNav = function(content, btn) {
+		var self = this,
+			$navContainer = self.$navContainer,
+			$staggerItems = self.$navMenu.children('li');
+
+		content.addClass(self.modifiers.opened);
+		btn.addClass(self.modifiers.active);
+
+		$navContainer.css({
+			'-webkit-transition-duration': '0s',
+			'transition-duration': '0s'
+		});
+
+		var navTween = self.navTween,
+			insideElementsTween = new TimelineMax();
+
+		insideElementsTween
+			.set($staggerItems, {autoAlpha:0, scale:0.8}, 0.05);
+
+		if (navTween.progress() != 0 && !navTween.reversed()) {
+			navTween.reverse();
+		} else {
+			navTween
+				.play()
+				.eventCallback('onComplete', function () {
+					insideElementsTween
+						.staggerTo($staggerItems, 0.3, {autoAlpha:1, scale:1, ease:Cubic.easeInOut}, 0.08);
+				});
+		}
+
+		self.showOverlay();
+	};
+
+	// close nav
+	MainNavigation.prototype.closeNav = function(content, btn) {
+		var self = this,
+			$navContainer = self.$navContainer,
+			$buttonMenu = self.$btnMenu;
+
+		content.removeClass(self.modifiers.opened);
+		btn.removeClass(self.modifiers.active);
+		self.showOverlay(false);
+
+		if ($buttonMenu.is(':hidden')) {
+			$navContainer.attr('style','');
+			return false;
+		}
+
+		var navTween = self.navTween;
+
+		navTween.reverse();
+	};
+
+	// clearing inline styles
+	MainNavigation.prototype.clearStyles = function() {
+		var self = this,
+			$navContainer = self.$navContainer;
+
+		//clear on horizontal resize
+		$(window).on('resize', function () {
+			if($navContainer.attr('style')){
+				$navContainer.attr('style','');
+			}
+		});
+	};
+
+	window.MainNavigation = MainNavigation;
+
+}(jQuery));
+
+function mainNavigationInit(){
+	var $container = $('.nav');
+	if(!$container.length){ return; }
+	new MainNavigation({
+		navContainer: $container,
+		animationSpeed: 300,
+		overlayBoolean: true,
+		overlayAlpha: 0.75
+	});
+}
+/*main navigation end*/
+
 /*drop language*/
 function languageEvents() {
 	$('.lang-current').on('click', function (e) {
@@ -55,31 +283,54 @@ function languageEvents() {
 }
 /*drop language end*/
 
+/*sliders*/
+function slidersInit() {
+	// adt slider
+	var $adtSlider = $('.adt-slider');
+	if($adtSlider.length){
+		$adtSlider.on('init', function () {
+			$(this).css({'visibility':'visible'});
+		});
+		$adtSlider.slick({
+			fade: true,
+			autoplay: true,
+			autoplaySpeed: 10000,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			speed: 500,
+			infinite: true,
+			dots: false,
+			arrows: true
+		});
+	}
+}
+/*sliders end*/
+
 /*map init*/
 var largePinMap = 'img/map-pin.png';
 
 var localObjects = [
 	[
-		{lat: 32.9122, lng: -96.7391}, //coordinates of marker
+		{lat: 50.4838, lng: 23.5353}, //coordinates of marker
 		{latBias: 0.0020, lngBias: 0}, //bias coordinates for center map
 		largePinMap, //image pin
-		15,
+		5,
 		{
-			title: 'USA & Canada',
-			address: '<b>Address:</b> <div>9330 LBJ Freeway, Suite 900 <br> Dallas, TX 75243 <br> PO Box 647, LIghtfoot, VA 23090</div>',
-			phone: '<b>Tel.:</b> <div><a href="tel:2145613922">(214) 561-3922</a></div>',
-			works: '<b>E-mail:</b> <div><a href="mailto:info@aztoys.com">info@aztoys.com</a></div>'
+			title: 'Названиеу',
+			address: '<b>Адрес:</b> <div>214013, г. Смоленск, <br> ул. Кирова, д. 22Б</div>',
+			phone: '<b>Тел.:</b> <div><a href="tel:2145613922">+37517 500-20-02</a></div>',
+			works: '<b>E-mail:</b> <div><a href="mailto:info@aztoys.com">info@tenzor.su</a></div>'
 		}
 	],[
-		{lat: 59.4384, lng: 24.7340}, //coordinates of marker
+		{lat: 50.4838, lng: 23.5353}, //coordinates of marker
 		{latBias: 0.0020, lngBias: 0}, //bias coordinates for center map
 		largePinMap, //image pin
-		15,
+		5,
 		{
-			title: 'USA & Canada',
-			address: '<b>Address:</b> <div>Toompuiestee 35, korrus 2, <br> Tallinn 10133, Estonia</div>',
-			phone: '<b>Tel.:</b> <div><a href="tel:003725183088"></div>',
-			works: '<b>E-mail:</b> <div><a href="mailto:info@aztoys.com">info@aztoys.com</a></div>'
+			title: 'Названиеу',
+			address: '<b>Адрес:</b> <div>214013, г. Смоленск, <br> ул. Кирова, д. 22Б</div>',
+			phone: '<b>Тел.:</b> <div><a href="tel:2145613922">+37517 500-20-02</a></div>',
+			works: '<b>E-mail:</b> <div><a href="mailto:info@aztoys.com">info@tenzor.su</a></div>'
 		}
 	]
 ];
@@ -108,7 +359,7 @@ function mapMainInit(){
 
 	if($(elementById[0]).length){
 		mapOptions = {
-			zoom: 15,
+			zoom: localObjects[0][3],
 			center: mapCenter(0),
 			styles: styleMap,
 			mapTypeControl: false,
@@ -131,7 +382,7 @@ function mapMainInit(){
 
 	if($(elementById[1]).length){
 		mapOptions = {
-			zoom: 15,
+			zoom: localObjects[1][3],
 			center: mapCenter(1),
 			styles: styleMap,
 			mapTypeControl: false,
@@ -330,7 +581,9 @@ function footerBottom(){
 $(document).ready(function(){
 	placeholderInit();
 	printShow();
+	mainNavigationInit();
 	languageEvents();
+	slidersInit();
 	mapMainInit();
 	if(DESKTOP){
 		customSelect($('select.cselect'));
